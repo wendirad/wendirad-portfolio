64e79319ed2281a0821d5563f12aa254f5d4907e
hunderaweke <hunderaweke@gmail.com>
2023-06-16 13:35:23 +0300

    updated the testimonial part

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <swiper 
+      :slidesPerView="1"
+      :breakpoints="breakpoints"
+      :spaceBetween="30"
+      :pagination="{
+        clickable: true,
+      }"
+      :autoplay="{
+        delay: 2000,
+        disableOnInteraction: false,
+      }"
+      :keyboard="{
+        enabled:true,
+      }"
+      :modules="modules"
+      class="m-10"
+    >
+      <swiper-slide class='text-center bg-slate-300 p-5 rounded-md mb-10' v-for="(slide,index) in slides" :key="index">
+        <p>
+              {{ slide.description }}
+            </p>
+        <div class="text-end mt-3"><h1 class="text-lg font-bold">
+            {{ slide.author }}
+          </h1> 
+        <p class="font-extralight text-sm">{{ slide.profession }}</p> 
+        <img :src="slide.img" class="w-12 h-12 absolute rounded-full right-3"></div>
+      </swiper-slide>
+    </swiper>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import {Autoplay,Keyboard,Pagination} from 'swiper'
+import 'swiper/css';
+import 'swiper/css/pagination';
+let testimonials = require('./testimonials.json')
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Swiper, SwiperSlide },
+  data() {
+    return {
+      slides: testimonials,
+    }
+  },
+  setup(){
+    return {
+      modules: [Pagination, Autoplay, Keyboard],
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+        },
+        780: {
+          slidesPerView: 3,
+        },
+      }
+    }
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.swiper-pagination-bullet {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: #656161;
+  opacity: 0.5;
+  transition: opacity 0.2s ease-out;
+}
+
+.swiper-pagination-bullet-active {
+  opacity: 1;
+  background-color: #000;
+}
+
+.swiper-pagination-bullet-active::after {
+  content: '';
+  display: block;
+  width: 10px;
+  height: 10px;
+  transform: rotate(45deg);
+  background-color: #000;
 }
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode:'class',
+  theme: {
+    extend: {
+      fontFamily:{
+        'primary':['Gayathri'],
+        'hero':['Monomaniac One'],
+        'title':['Roboto Mono'],
+      },
+      colors:{
+        'primary-light':'#FFFDFD',
+        'secondary-light':'#11151C',
+        'primary-dark':"#11151C",
+        'secondary-dark':'#2E394C',
+      },
+      fontSize:{
+        '4xl': '2.25rem',  
+        '5xl': '3rem',    
+        '6xl': '4rem',
+      },
+      screens: {
+        'phone': {'max': '639px'},
+        'tablet': {'min': '640px', 'max': '1023px'},
+        'desktop': {'min': '1024px'},
+      },
+      backgroundImage:{
+        'hero-image-light': "url('frontend/img/hero-bg.png')",
+        'hero-image-dark':"url('frontend/img/hero-bg-dark.png)",
+      },
+    },   
+  },
+  plugins: [],
+}
+
